35efaadafd6c9da89d9fc648df6a5a618e573dc4
elluna <stella.piebrock@outlook.de>
2023-09-12 15:41:04 +0200

    import errors solves for now by putting everything in one file

--- Arm_Controller.py
+++ Arm_Controller.py
@@ -1,10 +1,9 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import tkinter as tk
 
 from dynamixel_sdk import *  # Uses Dynamixel SDK library
-
-from GUI import get_degrees
 
 if os.name == 'nt':
     import msvcrt
@@ -134,8 +133,59 @@ def moving(motor_id, degree):
 # Checks input number
 def start_moving(event):
     for motor in DXL_ID:
-        moving(motor, get_degrees()[motor-1])
+        moving(motor, get_degrees()[motor - 1])
 
+
+# Initialize window
+root = tk.Tk()
+
+# Create text output
+l1 = tk.Label(root, text="Robotic Arm Controller")
+l2 = tk.Label(root, text="DXL1:")
+l3 = tk.Label(root, text="DXL2:")
+l4 = tk.Label(root, text="DXL3:")
+
+# Display text in GUI
+l1.grid(row=0, column=0)
+l2.grid(row=1, column=0)
+l3.grid(row=2, column=0)
+l4.grid(row=3, column=0)
+
+# Create first entry field
+# Call variablenname.get() to get the degrees
+entry1 = tk.StringVar()
+field1 = tk.Entry(root, textvariable=entry1)
+
+# Create second entry field
+entry2 = tk.StringVar()
+field2 = tk.Entry(root, textvariable=entry2)
+
+# Create third entry field
+entry3 = tk.StringVar()
+field3 = tk.Entry(root, textvariable=entry3)
+
+# Display entry fields
+field1.grid(row=1, column=1)
+field2.grid(row=2, column=1)
+field3.grid(row=3, column=1)
+
+
+# Function to get the entries
+def get_degrees():
+    degrees = [int(field1.get()), int(field2.get()), int(field3.get())]
+    return degrees
+
+
+# Create OK button to start movement
+MovingBtn = tk.Button(root, text="OK")
+MovingBtn.grid(row=1, column=2)
+# Call moving function
+MovingBtn.bind('<ButtonPress-1>', start_moving)
+
+tk.Button(root, text="Quit", command=root.destroy).grid(column=1, row=4)
+
+# Infinite loop which can be terminated by keyboard or mouse interrupt
+root.mainloop()
 
 # Disable Dynamixel Torque for each motor
 # DXL_ID is an array which includes the different Dynamixel motor ID's
